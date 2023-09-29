7607b1425b38cfac48674740511b76cb27d9829c
oleksandr.molodchyk <oleksandr.molodchyk@student.tuke.sk>
2023-09-29 17:15:22 +0200

    17:15

--- content.js
+++ content.js
@@ -1,29 +1,29 @@
 // content.js
 
-// Function to reduce the size or opacity of the specified element
-function reduceElementSize(element) {
-  element.style.height = "10px"; // Adjust as needed
-  element.style.width = "10px"; // Adjust as needed
-  element.style.opacity = "0.2"; // Adjust as needed
+// Function to hide the specified element
+function hideElement(element) {
+  element.style.display = "none";
 }
 
 // Function to block the Discord element
 function blockDiscordElement() {
-  setTimeout(() => {
-    const discordElement = document.querySelector("searchBar-jGtisZ");
-    if (discordElement) {
-      reduceElementSize(discordElement);
-    }
-  }, 2000); // Delay execution for 2 seconds (adjust the delay as needed)
+  // Locate and block the specified element on Discord channels
+  const discordElement = document.querySelector("div.searchBar-jGtisZ");
+  if (discordElement) {
+    hideElement(discordElement); // Hide the specified element
+  }
 }
 
+// Check if the current page is a Discord channel page
 if (window.location.href.startsWith("https://discord.com/channels/")) {
+  // Initial call to block the specified element
   blockDiscordElement();
 }
 
 const observer = new MutationObserver((mutationsList, observer) => {
   for (const mutation of mutationsList) {
     if (mutation.type === 'childList') {
+      // Call the function to check and block the specified element whenever DOM changes occur
       blockDiscordElement();
     }
   }
